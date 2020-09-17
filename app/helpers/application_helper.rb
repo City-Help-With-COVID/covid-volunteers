f2baaef7687f4a15750ac9860891cbdf4c0158f7
Camden Rogers <camdenrogers@att.net>
2020-09-17 14:25:57 -0400

    Made more configurable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,9 @@ module ApplicationHelper
   def nav_link_active_class(variant = 'DESKTOP')
     case variant
     when 'DESKTOP'
-      'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out ml-4 text-center'
+      'inline-flex items-center px-1 pt-1 border-b-2 border-<%= PRIMARY_COLOR %>-500 text-sm leading-5 text-gray-900 focus:outline-none focus:border-<%= PRIMARY_COLOR %>-700 transition duration-150 ease-in-out ml-4 text-center'
     when 'MOBILE'
-      'block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
+      'block pl-3 pr-4 py-2 border-l-4 border-<%= PRIMARY_COLOR %>-500 text-base text-<%= PRIMARY_COLOR %>-700 bg-<%= PRIMARY_COLOR %>-50 focus:outline-none focus:text-<%= PRIMARY_COLOR %>-800 focus:bg-<%= PRIMARY_COLOR %>-100 focus:border-<%= PRIMARY_COLOR %>-700 transition duration-150 ease-in-out'
     end
   end
 
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -31,17 +31,17 @@ ALL_NUMBER_OF_VOLUNTEERS = [ '1-10', '10-50', '50-100', '100+' ]
 ALL_LEVELS_OF_URGENCY = [ 'Extremely urgent', 'Very urgent', 'Moderately urgent', 'Somewhat urgent', 'Not urgent' ]
 ALL_LEVELS_OF_EXPOSURE = [ 'I', 'II', 'III', 'IV', 'V' ]
 REMOTE_LOCATION = 'Remote'
-CITY_NAME = 'Athens'
-STATE_NAME = 'Georgia'
+CITY_NAME = 'City'
+STATE_NAME = 'State'
 ALL_LOCATIONS = [ "#{CITY_NAME} (on site)", "Other #{STATE_NAME} locales (on site)", REMOTE_LOCATION ]
 DOMAIN_NAME = 'ThisCityHelpWithCovid.com'
 AFFILIATE_EMAIL = 'info@thiscityhelpwithcovid.com'
-PRIMARY_COLOR = 'red'
+PRIMARY_COLOR = 'indigo'
 PROJECTS_SUBHEADER = "This is a #{CITY_NAME}-wide partnership platform, where #{CITY_NAME} residents can volunteer (in-person or remotely) and local non-profits and government can post volunteer needs. Let us unite and fight the pandemic together!"
-INSTAGRAM_LINK = 'https://www.instagram.com/athensmutualaid.net_updates/?hl=en'
-FACEBOOK_LINK = 'https://www.facebook.com/Athens-help-With-Covid-19-103765018035330'
-TWITTER_LINK = 'https://twitter.com/AthGaHelpwCovid'
-NEWSLETTER_LINK = 'http://eepurl.com/g3hEa5'
+INSTAGRAM_LINK = ''
+FACEBOOK_LINK = ''
+TWITTER_LINK = ''
+NEWSLETTER_LINK = ''
 ALL_PROJECT_TYPES = [
   'Contact tracing',
   'Reduce spread',
